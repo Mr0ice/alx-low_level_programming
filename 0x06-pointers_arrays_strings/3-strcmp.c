42fba982c49c4dd79e1724d702215bb9ea80ebda
Mr0ice <okpoduprosper@gmail.com>
2023-07-27 06:40:50 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * _strcnp - compare string values
+ * _strcmp - compare string values
  * @s1: input value
  * @s2: input value
+ *
  * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
@@ -14,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] = s2[i]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
